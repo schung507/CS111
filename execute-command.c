d38a0c7cb23c6c7aad87edf95560b11f233337fd
Lynn Hao Thi Tran <lynn@lnxsrv02.seas.ucla.edu>
2015-01-22 03:45:58 -0800

    Added recursive execute function for if, while, until, and sequence.

--- execute-command.c
+++ execute-command.c
@@ -23,6 +23,12 @@
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
+void r_execute(command_t c, int in, int out);
+void execute_if(command_t c, int in, int out);
+void execute_woru(command_t c, int in, int out);
+void execute_sequence(command_t c, int in, int out);
+
+
 int
 prepare_profiling (char const *name)
 {
@@ -44,4 +50,48 @@ execute_command (command_t c, int profiling)
 {
   /* FIXME: Replace this with your implementation, like 'prepare_profiling'.  */
   error (1, 0, "command execution not yet implemented");
+}
+
+void r_execute(command_t c, int in, int out) {
+  enum command_type type = c->type;
+  
+  switch(type) {
+
+  case IF_COMMAND: 
+    execute_if(c, in, out);
+  case WHILE_COMMAND:
+  case UNTIL_COMMAND:
+    execute_woru(c, in, out);
+  case SEQUENCE_COMMAND:
+   execute_sequence(c, in, out);
+  }
+}
+
+void execute_if(command_t c, int in, int out) {
+
+  if (r_execute(c->u.command[0]->status) == 0) {
+    r_execute(c->u.command[1], in, out);
+    c->status = c->u.command[1]->status;
+  }
+  else if (c->u.command[2] != NULL) {
+    r_execute(c->u.command[2], in, out);
+    c->status = c->u.command[2]->status;
+  }
+  c->status = c->u.command[0]->status;
+}
+
+void execute_woru(command_t c, int in, int out) {
+
+  while (r_execute(c->u.command[0]->status) == 0) {
+    r_execute(c->u.command[1], in, out);
+    c->status = c->u.command[1]->status;
+  }
+  c->status = c->u.command[0]->status;
+}
+
+void execute_sequence(command_t c, int in, int out) {
+  
+  r_execute(c->u.command[0], in, out);
+  r_execute(c->u.command[1], in, out);
+  c->status = c->u.command[1]->status;
 }
