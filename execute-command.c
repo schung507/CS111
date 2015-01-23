1a7c92dac8fb8867dc8edd8c280e893b38a74b19
Lynn Hao Thi Tran <lynn@lnxsrv04.seas.ucla.edu>
2015-01-23 00:40:58 -0800

    Added recursive execute calls. Pipe hanging.

--- execute-command.c
+++ execute-command.c
@@ -19,17 +19,24 @@
 #include "command-internals.h"
 
 #include <error.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /* FIXME: You may need to add #include directives, macro definitions,
-   static function definitions, etc.  
+   static function definitions, etc.  */
 
 void r_execute(command_t c, int in, int out);
 void execute_if(command_t c, int in, int out);
 void execute_while(command_t c, int in, int out);
 void execute_until (command_t c, int in, int out);
-void execute_sequence(command_t c, int in, int out);
+void execute_sequence(command_t c, int in, int out); 
 void execute_simple(command_t c, int in, int out);
-*/
+/*void execute_subshell(command_t c, int in, int out);*/
+void execute_pipe(command_t c, int in, int out); 
 
 int
 prepare_profiling (char const *name)
@@ -51,27 +58,49 @@ void
 execute_command (command_t c, int profiling)
 {
   /* FIXME: Replace this with your implementation, like 'prepare_profiling'.  */
-  error (1, 0, "command execution not yet implemented");
+  r_execute(c, -1, -1); //no file descriptors are yet set
 }
-/*
+
 void r_execute(command_t c, int in, int out) {
   enum command_type type = c->type;
   
   switch(type) {
 
-  case IF_COMMAND: 
+    case IF_COMMAND: 
     execute_if(c, in, out);
+    break;
   case WHILE_COMMAND:
+    execute_while(c, in, out);
+    break;
   case UNTIL_COMMAND:
-    execute_woru(c, in, out);
+    execute_until(c, in, out);
+    break;
   case SEQUENCE_COMMAND:
-   execute_sequence(c, in, out);
+    execute_sequence(c, in, out);
+    break; 
+  case SIMPLE_COMMAND:
+    execute_simple(c, in, out);
+    break;
+  case SUBSHELL_COMMAND:
+    //execute_subshell(c, in, out);
+    break;
+  case PIPE_COMMAND:
+    execute_pipe(c, in, out);
+    break;
   }
+
 }
 
 void execute_if(command_t c, int in, int out) {
 
-  if (r_execute(c->u.command[0]->status) == 0) {
+  if (c->input != NULL)
+    in = open(c->input, O_RDONLY|O_CREAT);
+  if (c->output != NULL)
+    out = open(c->output, O_WRONLY|O_CREAT|O_TRUNC);
+
+  r_execute(c->u.command[0], in, out);
+
+  if (c->u.command[0]->status == 0) {
     r_execute(c->u.command[1], in, out);
     c->status = c->u.command[1]->status;
   }
@@ -80,11 +109,20 @@ void execute_if(command_t c, int in, int out) {
     c->status = c->u.command[2]->status;
   }
   c->status = c->u.command[0]->status;
+
 }
+
 
 void execute_while(command_t c, int in, int out) {
 
-  while (r_execute(c->u.command[0]->status) != 0) {
+  if (c->input != NULL)
+    in = open(c->input, O_RDONLY|O_CREAT);
+  if (c->output != NULL)
+    out = open(c->output, O_WRONLY|O_CREAT|O_TRUNC);
+
+  r_execute(c->u.command[0], in, out);
+
+  while (c->u.command[0]->status != 0) {
     r_execute(c->u.command[1], in, out);
     c->status = c->u.command[1]->status;
   }
@@ -93,7 +131,14 @@ void execute_while(command_t c, int in, int out) {
 
 void execute_until(command_t c, int in, int out) {
   
-  while (r_execute(c->u.command[0]->status) == 0) {
+  if (c->input != NULL)
+    in = open(c->input, O_RDONLY|O_CREAT);
+  if (c->output != NULL)
+    out = open(c->output, O_WRONLY|O_CREAT|O_TRUNC);
+
+  r_execute(c->u.command[0], in, out);
+
+  while (c->u.command[0]->status == 0) {
     r_execute(c->u.command[1], in, out);
     c->status = c->u.command[1]->status;
   }
@@ -109,5 +154,72 @@ void execute_sequence(command_t c, int in, int out) {
 
 void execute_simple(command_t c, int in, int out) {
 
+  if (c->input != NULL)
+    in = open(c->input, O_RDONLY|O_CREAT);
+  if (c->output != NULL)
+    out = open(c->output, O_WRONLY|O_CREAT|O_TRUNC);
+
+  int status;
+
+  pid_t p = fork();
+  if (p == 0) {   //We are in the child
+    execvp(c->u.word[0], c->u.word);
+    perror("execvp returned");    //execvp returned, but if successful should destroy itself
+  }
+  else if (p < 0) {
+    perror("Failed to fork.");
+  }
+  else {
+    waitpid(p, &status, 0);
+  }
+
+  if (WIFEXITED(&status))
+    c->status = WEXITSTATUS(&status);
+  else
+    c->status = status;
 }
-*/
+
+void execute_pipe(command_t c, int in, int out){
+
+  int pid1, pid2, status;
+  int fd[2];
+
+  pipe(fd);
+
+  switch (pid1 = fork()){
+ 
+  case 0:
+
+    switch (pid2 = fork()) {
+
+    case 0: /* child */
+     
+      close(fd[0]);/*does not need this end of the pipe */
+      r_execute(c->u.command[0], in, fd[1]);
+      exit(command_status(c->u.command[0]));
+
+     
+    default: /* parent*/
+
+      close(fd[1]);
+      r_execute(c->u.command[1], fd[0], out);
+      exit(command_status(c->u.command[1]));
+
+    case -1:
+      perror("Failed to fork second time");
+      exit(1);
+    }
+
+  default:/*parent*/
+
+    waitpid( pid1, &status, 0);
+    c->status = command_status(c->u.command[1]);
+    break;
+  case -1:
+    perror("Failed fork first time");
+    exit(1);
+  }
+
+  exit(0);
+  
+}
