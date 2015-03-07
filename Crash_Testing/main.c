cb0f1b716549b30a14669ee4567dc44fb448be99
Stella Chung <sassystellz@sassystellz-laptop.(none)>
2015-03-06 17:12:38 -0800

    boooo

--- Crash_Testing/main.c
+++ Crash_Testing/main.c
@@ -10,7 +10,7 @@ main(int argc, char **argv){
   fd = open("./test/world.txt", O_RDWR);
   //fd2 = open("./test/thelink", O_RDWR | O_CREAT);
 
-  ioctl(fd, IOCTL_CRASH, atoi(argv[1]));
+  ioctl(fd, IOCTL_NWRITES_TO_CRASH, atoi(argv[1]));
   // write(fd, "yas yas yas", 12);
   //link("./test/world.txt", "./test/thelink");
  
