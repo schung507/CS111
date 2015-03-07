450a36cabcc230a9ffd534af3df85e46031179ea
Stella Chung <sassystellz@sassystellz-laptop.(none)>
2015-03-06 22:09:56 -0800

    watever

--- Crash_Testing/set_crash.c
+++ Crash_Testing/set_crash.c
@@ -5,10 +5,11 @@
 
 main(int argc, char **argv){
   int fd;
-  // int fd2;
-  printf("%d",  atoi(argv[1]));
+  
+  // printf("%d",  atoi(argv[1]));
   fd = open("./test/crash.txt", O_RDONLY | O_CREAT);
 
+  //set nwrites_to_crash
   ioctl(fd, IOCTL_NWRITES_TO_CRASH, atoi(argv[1]));
   // write(fd, "yas yas yas", 12);
   //link("./test/world.txt", "./test/thelink");
